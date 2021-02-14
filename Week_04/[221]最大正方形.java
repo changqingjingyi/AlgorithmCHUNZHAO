30b5b2def664c427426d03369797c4d770ca55f9
ptc <1298844520@qq.com>
2021-02-14 10:00:42 +0800

    week04

---
+++ Week_04/[221]最大正方形.java
@@ -0,0 +1,66 @@
+//在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"]
+//,["1","0","0","1","0"]]
+//输出：4
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：matrix = [["0","1"],["1","0"]]
+//输出：1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：matrix = [["0"]]
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= m, n <= 300 
+// matrix[i][j] 为 '0' 或 '1' 
+// 
+// Related Topics 动态规划 
+// 👍 665 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        // base condition
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return 0;
+
+        int height = matrix.length;
+        int width = matrix[0].length;
+        int maxSide = 0;
+
+        // 相当于已经预处理新增第一行、第一列均为0
+        int[][] dp = new int[height + 1][width + 1];
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                if (matrix[row][col] == '1') {
+                    dp[row + 1][col + 1] = Math.min(Math.min(dp[row + 1][col], dp[row][col + 1]), dp[row][col]) + 1;
+                    maxSide = Math.max(maxSide, dp[row + 1][col + 1]);
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ Week_04/[647]回文子串.java
@@ -0,0 +1,71 @@
+//给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。 
+//
+// 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入："abc"
+//输出：3
+//解释：三个回文子串: "a", "b", "c"
+// 
+//
+// 示例 2： 
+//
+// 输入："aaa"
+//输出：6
+//解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa" 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 输入的字符串长度不会超过 1000 。 
+// 
+// Related Topics 字符串 动态规划 
+// 👍 483 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+class Solution {
+    public int countSubstrings(String s) {
+        // 动态规划法
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int ans = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            for (int i = 0; i <= j; i++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+class Solution {
+    public int countSubstrings(String s) {
+        // 动态规划法
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int ans = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            for (int i = 0; i <= j; i++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
